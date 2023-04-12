ed597af592698396bcdf35f5c927eb71073aba4a
Justin Papreck <justin.papreck@gmail.com>
2023-04-12 14:05:52 -0700

    Started SQL Applications work

--- Exercise Files/ch01/sql_scripts/load_ecommerce_tabales.sql
+++ Exercise Files/ch01/sql_scripts/load_ecommerce_tabales.sql
@@ -1,4 +1,8 @@
-COPY product_orders FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/product_orders.csv' DELIMITER ',' CSV HEADER;
-COPY products FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/products.csv' DELIMITER ',' CSV HEADER;
-COPY orders FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/orders.csv' DELIMITER ',' CSV HEADER;
-COPY customers FROM '/Users/danielsullivan/LinkedIn Learning/advanced_sql_developers/data/customers.csv' DELIMITER ',' CSV HEADER;
+COPY product_orders FROM '\Users\justi\OneDrive\Desktop\Analytics\AdventureWorks\Exercise Files\ch01\data\product_orders.csv' DELIMITER ',' CSV HEADER;
+COPY products FROM '\Users\justi\OneDrive\Desktop\Analytics\AdventureWorks\Exercise Files\ch01\data\products.csv' DELIMITER ',' CSV HEADER;
+COPY orders FROM '\Users\justi\OneDrive\Desktop\Analytics\AdventureWorks\Exercise Files\ch01\data\orders.csv' DELIMITER ',' CSV HEADER;
+COPY customers FROM '\Users\justi\OneDrive\Desktop\Analytics\AdventureWorks\Exercise Files\ch01\data\customers.csv' DELIMITER ',' CSV HEADER;
+
+
+
+
